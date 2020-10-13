e3094674e97fd99a24b302490c37946d66a76071
RJ HANIF AHMED <66148227+MaxxRider@users.noreply.github.com>
2020-10-13 08:43:16 +0530

    Add files via upload

---
+++ madarchoot/config.py
@@ -0,0 +1,14 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1300536378:AAERF5bDDmqjhmIA6hzYN6FQOESSTuKrRGM"
+  APP_ID = 1737036
+  API_HASH = "d0482a809423646493e805dd13ee750b"
+  AUTH_CHANNEL = [-1001454070177, 1161316820]
+  INDEX_LINK = https://test.max.wiz
+  YTDL_COMMAND = ytdl@Upload_As_File_bot
+  LEECH_COMMAND = leech@Upload_As_File_bot
+  CLEAR_THUMBNAIL = clearthumbnail@Upload_As_File_bot
+  SAVE_THUMBNAIL = savethumbnail@Upload_As_File_bot
+  OWNER_ID = 1161316820
+  
